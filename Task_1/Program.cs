1adee710089e855bd4f5960d82f5229f72634880
Roman Safroshkin <romasafr@gmail.com>
2021-09-24 19:00:59 +0300

    Успел только две задачи, на большее пока времени к сожалению нет. Читаю все ваши пояснения к  проверке - очень помогают! Написал по аналогии с вашим кодом решение для 4 задачи из предыдущего урока - разобрался) Спасибо вам огромное за ваш труд

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Task_1
 {
+
+    public delegate double Func(double a, double x);
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Table(Func F, double a, double x, double b)
         {
+            Console.WriteLine("----- A ------- X -------- Y -----");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} | {2,8:0.000} |", a, x, F(a, x));
+                x += 1;
+            }
+            Console.WriteLine("-----------------------------------");
+        }
+        public static double PowFunc(double a, double x)
+        {
+            return a * x * x;
+        }
+
+        public static double SinFunc(double a, double x)
+        {
+            return a * Math.Sin(x);
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Значения функции a*x^2:");
+            Table(new Func(PowFunc), -1.5, -2, 2);
+
+            Console.WriteLine("Значения функции a*sin(x):");
+            Table(new Func(SinFunc), 3, -2, 2);
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Task_2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        delegate double Func(double a, double x);
+
+        private static double PowFunc(double a, double x)
+        {
+            return a * x * x;
+        }
+
+        private static double SinFunc(double a, double x)
+        {
+            return a * Math.Sin(x);
+        }
+
+        public static void FuncAccept(string fileName, double a, double b, double c, int FN)
+        {
+            
+            Func onFunc;
+            if (FN == 1)
+            {
+                onFunc = PowFunc;
+            }
+            else 
+            {
+                onFunc = SinFunc;
+            }
+
+            FileStream fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter binReader = new BinaryWriter(fileStream);
+            double x = a;
+            while (x <= b)
+            {
+                binReader.Write(onFunc.Invoke(a, x));
+                x += c;
+            }
+            binReader.Close();
+            fileStream.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader binReader = new BinaryReader(fileStream);
+           
+            double F_min = double.MaxValue;
+            double d;
+
+            for (int i = 0; i < fileStream.Length / sizeof(double); i++)
+            {
+                d = binReader.ReadDouble();
+                if (d < F_min) F_min = d;
+            }
+            binReader.Close();
+            fileStream.Close();
+            return F_min;
+        }
+
+        public static int[] Menu()
+        {
+            int[] var = new int[3];
+
+            Console.Write("Выберите функцию:\n Цифра 1 - a*x^2;\n Цифра 2 - a*sin(x):\n ");
+            var[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите отрезок, на котором будем искать минимум.\n" +
+                "Первое число - начало отрезка, второе число - конец отрезка:\n ");
+            var[1] = Convert.ToInt32(Console.ReadLine());
+            var[2] = Convert.ToInt32(Console.ReadLine());
+
+            return var;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] variables = Menu();
+
+            FuncAccept("data.bin", variables[1], variables[2], 0.5, variables[0]);
+            Console.WriteLine(Load("data.bin"));
+
+            Console.ReadKey();
+        }
+    }
+}
